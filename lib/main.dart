5ae4e06e26cdbd40c99ca1888c41430a1fe42973
Daniel-TheProgrammer <njid18753@gmail.com>
2022-06-13 14:32:03 +0100

    added explicit show combinator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' show BuildContext, Key, MaterialApp, StatelessWidget, Widget, runApp;
 
 import 'screens/loading.dart';
 
